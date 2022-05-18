896acdecd2703a09686b3945ec6b3099cc855809
sibtain.raza <sibtain.raza@10pearls.com>
2022-05-18 11:29:02 +0500

    Compose : Box

--- app/src/main/java/com/smsrn/composepractice/MainActivity.kt
+++ app/src/main/java/com/smsrn/composepractice/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.smsrn.composepractice.ui.theme.ComposePracticeTheme
 import com.smsrn.composepractice.ui.theme.Typography
 
@@ -26,16 +29,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .height(500.dp)
-                            .width(500.dp)
-                            .background(Color.LightGray),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
-                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-                        CustomItem(weight = 1f)
-                    }
+                    Greeting()
                 }
             }
         }
@@ -43,27 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
-    Surface(
-        modifier = Modifier
-            .height(50.dp)
-            .width(50.dp)
-            .weight(weight),
-        color = color
-    ) {}
+fun Greeting() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .background(Color.Green)
+            )
+            Text(text = "I Love Android!", fontSize = 40.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Row(
-            Modifier.height(500.dp),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-            CustomItem(weight = 1f)
-        }
+        Greeting()
     }
 }
