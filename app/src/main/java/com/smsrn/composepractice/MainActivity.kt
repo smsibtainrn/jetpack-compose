8b00ca9cc53726979fb64774aba3e0b2139b9061
sibtain.raza <sibtain.raza@10pearls.com>
2022-05-18 11:01:11 +0500

    Compose : Column and Column Scope.

--- app/src/main/java/com/smsrn/composepractice/MainActivity.kt
+++ app/src/main/java/com/smsrn/composepractice/MainActivity.kt
@@ -3,14 +3,17 @@ package com.smsrn.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.smsrn.composepractice.ui.theme.ComposePracticeTheme
 import com.smsrn.composepractice.ui.theme.Typography
 
@@ -21,12 +24,17 @@ class MainActivity : ComponentActivity() {
             ComposePracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        CustomerText(text = "Sibtain")
+                    Column(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+                        CustomItem(weight = 1f)
                     }
                 }
             }
@@ -35,25 +43,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun CustomerText(text: String) {
-    Text(
-        text = text,
-        style = Typography.h5
-    )
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Column {
-            Greeting("Android")
-            CustomerText(text = "Sibtain")
+        Column(
+            Modifier.height(500.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
